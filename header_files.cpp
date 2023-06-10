ad07d874230e6445705655f5630934efb6982204
Samiksha <135847601+Samiksha14087@users.noreply.github.com>
2023-06-10 10:09:21 +0530

    Add files via upload

---
+++ header_files.cpp
@@ -0,0 +1,10 @@
+/*There are two types of header files:
+1.System header files - It comes with compiler
+2.User defined header files - It is written by the programmer*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!";
+}
